3a53f2a42969f087e82d573792a74e950e819f52
Jaques <jaquespd@gmail.com>
2020-03-31 20:37:32 -0300

    static heroku

---
+++ build/precache-manifest.b540b5e70a6afc2f6e98bdb38e8ec188.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8da134d153adb51ffd1175ff41dd80cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd067c56fd35853e4c74",
+    "url": "/static/js/2.2d6cd946.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.2d6cd946.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2cb3b8b2c6ae7369d08",
+    "url": "/static/js/main.3b481a0b.chunk.js"
+  },
+  {
+    "revision": "dbdcd991415e5b5e5b71",
+    "url": "/static/js/runtime-main.9dda2659.js"
+  },
+  {
+    "revision": "1631354331249158b5e9bcb163188909",
+    "url": "/static/media/account_balance-white-18dp.16313543.svg"
+  },
+  {
+    "revision": "41caef82491fe97e22b5d39e7b75a56b",
+    "url": "/static/media/alarm-white-18dp.41caef82.svg"
+  },
+  {
+    "revision": "fe29405d059aaf76d34b96388655bc67",
+    "url": "/static/media/email-white-18dp.fe29405d.svg"
+  },
+  {
+    "revision": "98871e6ea4ecb56ff30918275907d0bc",
+    "url": "/static/media/format_list_bulleted-white-18dp.98871e6e.svg"
+  },
+  {
+    "revision": "ece1f476af2eb625970c6a6e5fbbf402",
+    "url": "/static/media/home_work-white-18dp.ece1f476.svg"
+  },
+  {
+    "revision": "6d282ee2130812c1c019a93c568508f0",
+    "url": "/static/media/important_devices-white-18dp.6d282ee2.svg"
+  },
+  {
+    "revision": "4016db626cc6872bbb7758b3e903a7d1",
+    "url": "/static/media/library_books-white-18dp.4016db62.svg"
+  },
+  {
+    "revision": "851c7061a34f3b409d4ed0876af40324",
+    "url": "/static/media/phone_in_talk-white-18dp.851c7061.svg"
+  },
+  {
+    "revision": "5e7d524eadeede4ac0121b23c4f9c5eb",
+    "url": "/static/media/search.5e7d524e.svg"
+  },
+  {
+    "revision": "33bd2e426be453ad4ae520efdd17bd78",
+    "url": "/static/media/sync-white-18dp.33bd2e42.svg"
+  }
+]);
